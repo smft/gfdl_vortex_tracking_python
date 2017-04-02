e3a8aef06eaed0b27e6fc6e6753b8ab22fb12fd8
Qi Zhang <577203573@qq.com>
2017-04-02 16:00:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,21 +3,17 @@
 ###author : Qi Zhang ###
 #######"""NJU"""########
 
-file_path=/pandisk/2013_result/ctl/
-chmod u+x follow_GFDL_tracking_mpi.py
-sec_st_time=`date -d '2013-01-01 00:00:00 utc' +%s`
-sec_ed_time=`date -d '2013-12-31 00:00:00 utc' +%s`
-while [[ ${sec_st_time} -lt ${sec_ed_time} ]];
+file_names=`ls bwp*.dat`
+
+for file in ${file_names}:
 do
-t=`date -d '1970-01-01 '${sec_st_time}' sec utc' -u +'%Y%m%d%H'`
-YEAR=${t:0:4}
-MONTH=${t:4:2}
-DAY=${t:6:2}
-HOUR=${t:8:2}
-echo ${t}
-time mpirun -n 5 ./follow_GFDL_tracking_mpi.py << EOF > path_${YEAR}${MONTH}${DAY}${HOUR}
-${file_path}/wrfout_d01_${YEAR}-${MONTH}-${DAY}_${HOUR}:00:00.nc4
-${YEAR}${MONTH}${DAY}${HOUR}
+echo ${file}
+cat ${file} | while read line
+do
+mpirun -n 5 follow_GFDL_tracking_mpi.py << EOF
+${line}
+/pandisk/2013_result/no_restart_cfsr_satinfo
+:00:00.nc4
 EOF
-sec_st_time=$[${sec_st_time}+21600]
+done
 done
